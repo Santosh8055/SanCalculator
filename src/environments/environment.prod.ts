933e7b5db90afebab8527eb43194f6466b96bd4f
santosh.pasupunuri <santosh.pasupunuri@vistex.com>
2019-08-21 16:17:55 +0530

    adding firebase config to prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
+const firebase = {
+  apiKey: 'AIzaSyAUDAqBQxw_Kmu8pAyBwh6x_ywzpSB4Wds',
+  authDomain: 'sanpracticefire.firebaseapp.com',
+  databaseURL: 'https://sanpracticefire.firebaseio.com',
+  projectId: 'sanpracticefire',
+  storageBucket: 'sanpracticefire.appspot.com',
+  messagingSenderId: '782472128860',
+  appId: '1:782472128860:web:04a213d287a63f6f'
+};
 export const environment = {
-  production: true
+  production: true,
+  firebase
 };
