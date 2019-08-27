5741f8be258829dd9f0c7e6347f99878bd8734b9
santosh.pasupunuri <santosh.pasupunuri@vistex.com>
2019-08-27 17:11:44 +0530

    adding oktaAuth

--- projects/auth/src/app/app-routing.module.ts
+++ projects/auth/src/app/app-routing.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SearchComponent } from './search/search.component';
 import { EditComponent } from './edit/edit.component';
+import { OktaCallbackComponent, OktaAuthGuard } from '@okta/okta-angular';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
-  { path: 'search', component: SearchComponent },
+  { path: 'search', component: SearchComponent, canActivate: [OktaAuthGuard] },
+  { path: 'profile', component: ProfileComponent },
   { path: 'edit/:id', component: EditComponent },
-  { path: '', redirectTo: '/search', pathMatch: 'full' }
+  {
+    path: 'implicit/callback',
+    component: OktaCallbackComponent
+  }
 ];
 
 @NgModule({
--- projects/auth/src/app/app.module.ts
+++ projects/auth/src/app/app.module.ts
@@ -8,9 +8,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { EditComponent } from './edit/edit.component';
 
+import { OktaAuthModule } from '@okta/okta-angular';
+import { ProfileComponent } from './profile/profile.component';
+
+const config = {
+  issuer: 'https://dev-221277.okta.com/oauth2/default',
+  redirectUri: 'http://localhost:4200/implicit/callback',
+  clientId: '0oa17iict3wI3XdhD357'
+};
+
 @NgModule({
-  declarations: [AppComponent, SearchComponent, EditComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
+  declarations: [AppComponent, SearchComponent, EditComponent, ProfileComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule, OktaAuthModule.initAuth(config)],
   providers: [],
   bootstrap: [AppComponent]
 })
